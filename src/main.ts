49c41c07a07b3c987c35c416ed555bebf068092e
Federico Aloi <federico.aloi@gmail.com>
2022-10-14 10:19:12 -0300

    Alguito de documentación

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ const { token } = configuracionDelEntorno();
 const bot = new Telegraf(token);
 
 bot.start((ctx) => {
+  /* 
+    El contexto (ctx) contiene información sobre el chat desde donde se invocó al bot.
+    
+    - Se puede acceder, por ejemplo, a información de la persona que escribió el mensaje (ctx.from),
+    al mensaje en sí mismo (ctx.message) y varias cosas más.
+    - También tiene varios métodos que permiten realizar acciones: responder (ctx.reply), reenviar
+    el mensaje (ctx.forwardMessage), cambiar la foto del chat (ctx.setChatPhoto), etc.
+  */
   ctx.reply(`¡Hola ${ctx.from.first_name}!`);
 });
 
