4f64a3cbc7c196b8d92597a634db4aadd76c6f67
Federico Aloi <federico.aloi@gmail.com>
2022-10-14 11:00:41 -0300

    Ejemplos de varios métodos

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { configuracionDelEntorno } from './config';
 const { token } = configuracionDelEntorno();
 const bot = new Telegraf(token);
 
+// Define el comando /start
 bot.start(async (ctx) => {
   /* 
     El contexto (ctx) contiene información sobre el chat desde donde se invocó al bot.
@@ -16,13 +17,37 @@ bot.start(async (ctx) => {
   await ctx.reply(`¡Hola ${ctx.from.first_name}!`);
 });
 
+// Define el comando /help
 bot.help(async (ctx) => {
-  await ctx.reply('Todavía no sé para qué sirvo... 🤔');
+  await ctx.reply('Todavía no sé cuál es mi gracia... 🤔');
 });
 
+// Define el comando que especifiquemos - /numero, en este caso
+bot.command('numero', async (ctx) => {
+  const elegido = Math.floor(Math.random() * 100);
+  await ctx.replyWithMarkdownV2(
+    `Tu número de la fortuna es el *${elegido}* 🤑`,
+  );
+});
+
+// Reacciona al evento especificado - la recepción de un sticker, en este caso
+bot.on('sticker', async (ctx) => {
+  await ctx.reply('Lindo sticker', {
+    reply_to_message_id: ctx.message.message_id,
+  });
+});
+
+// Reacciona a los mensajes que cumplan con la expresión regular
+bot.hears(/Mi nombre es (\w+)/, async (ctx) => {
+  const nombre = ctx.match[1];
+  await ctx.reply(`👋 ¡Qué bonito verte de nuevo, ${nombre}!`);
+});
+
+// Inicializa el bot
 bot.launch();
 
 console.log('🤖 ¡Listo para responder mensajes!');
 
+// Baja el servidor cuando recibe la orden por la consola
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
